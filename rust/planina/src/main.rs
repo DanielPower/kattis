18baf1ccf5093ad783b536c51401ddb83fe3a95c
Daniel Power <me@danielpower.ca>
2020-07-28 01:30:53 -0230

    Rust: planina

---
+++ rust/planina/src/main.rs
@@ -0,0 +1,8 @@
+use std::io::stdin;
+
+fn main() {
+    let mut buffer = String::new();
+    stdin().read_line(&mut buffer).unwrap();
+    let n = buffer.to_string().split_whitespace().next().unwrap().parse::<u32>().unwrap();
+    println!("{}", (2 + ((2 as u32).pow(n)-1)).pow(2));
+}
