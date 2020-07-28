577a7e66fa78a56ff18fe3bca86386d3d69cd2dd
Daniel Power <me@danielpower.ca>
2020-07-28 01:38:56 -0230

    Rust: r2

---
+++ rust/r2/src/main.rs
@@ -0,0 +1,10 @@
+use std::io::{stdin};
+
+fn main() {
+    let mut buffer = String::new();
+    stdin().read_line(&mut buffer).unwrap();
+    let mut values = buffer.split_whitespace();
+    let r1: i16 = values.next().unwrap().parse::<i16>().unwrap();
+    let s: i16 = values.next().unwrap().parse::<i16>().unwrap();
+    println!("{}", 2 * s - r1);
+}
